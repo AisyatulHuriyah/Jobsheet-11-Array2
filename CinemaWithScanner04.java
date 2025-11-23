c3ee4cacff4d874bf27c2e221940194e8d103e6c
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-11-23 17:13:51 +0700

    modify number 2

--- CinemaWithScanner04.java
+++ CinemaWithScanner04.java
@@ -9,20 +9,44 @@ public class CinemaWithScanner04 {
         String[][] audience = new String[4][2];
 
         while (true) {
-            System.out.print("Enter a name : ");
-            name = sc.nextLine();
-            System.out.print("Enter row number : ");
-            row = sc.nextInt();
-            System.out.print("Enter column number : ");
-            column = sc.nextInt();
+            System.out.println("\n=== CINEMA MENU ===");
+            System.out.println("1. Input audience data");
+            System.out.println("2. Show audience list");
+            System.out.println("3. Exit");
+            System.out.print("Choose menu (1/2/3): ");
+            int menu = sc.nextInt();
             sc.nextLine();
 
-            audience[row - 1][column - 1] = name;
-            System.out.print("Are there any other audience to be added? (y/n) : ");
-            next = sc.nextLine();
+            switch (menu) {
+                case 1:
+                    while (true) {
+                        System.out.print("Enter a name : ");
+                        name = sc.nextLine();
+                        System.out.print("Enter row number (1-4) : ");
+                        row = sc.nextInt();
+                        System.out.print("Enter column number (1-2) : ");
+                        column = sc.nextInt();
+                        sc.nextLine();
 
-            if (next.equalsIgnoreCase("n")) {
+                        audience[row - 1][column - 1] = name;
+                        System.out.print("Are there any other audience to be added? (y/n) : ");
+                        next = sc.nextLine();
+
+                        if (next.equalsIgnoreCase("n")) {
+                            break;
+                        }
+                    }
+                    break;
+
+                case 2:
+                System.out.println("Show audience list : ");
                 break;
+
+                case 3:
+                System.out.println("Program exit.");
+
+                default:
+                    System.out.println("Invalid menu. Please input again between 1-3");
             }
         }
     }
