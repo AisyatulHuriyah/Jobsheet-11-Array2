96e7ffc04d86f9fbac0dcd178b4a623f9def9015
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-11-23 21:19:27 +0700

    answer no 4

--- Numbers04.java
+++ Numbers04.java
@@ -11,5 +11,9 @@ public class Numbers04 {
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
         }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + myNumbers[i].length);
+        }
     }
 }
