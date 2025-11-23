1252aac710bf57425171d8289350a00b94af8e4a
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-11-23 21:17:38 +0700

    answer no 1

--- Numbers04.java
+++ Numbers04.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Numbers04 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
@@ -5,5 +7,9 @@ public class Numbers04 {
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 }
