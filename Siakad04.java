09fc984e7ea99d90aff614dcb8f166fdb9099cb7
AisyatulHuriyah <aisyatulhuriyah08@gmail.com>
2025-11-23 22:15:59 +0700

    modify number 1

--- Siakad04.java
+++ Siakad04.java
@@ -3,31 +3,49 @@ import java.util.Scanner;
 public class Siakad04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] score = new int[4][3];
 
-        for (int i = 0; i < score.length; i++) {
+        System.out.print("Enter number of students: ");
+        int student = sc.nextInt();
+
+        System.out.print("Enter number of courses: ");
+        int course = sc.nextInt();
+        sc.nextLine();
+
+        String[] courseNames = new String[course];
+        System.out.println("\nEnter course names:");
+        for (int j = 0; j < course; j++) {
+            System.out.print("Course " + (j + 1) + ": ");
+            courseNames[j] = sc.nextLine();
+        }
+
+        int[][] score = new int[student][course];
+
+        for (int i = 0; i < student; i++) {
             System.out.println("Enter scores for student #" + (i+1));
             double sumForEachStudent = 0;
 
-            for (int j = 0; j < score[i].length; j++) {
+            for (int j = 0; j < course; j++) {
                 System.out.print("Course #" + (j + 1) + " score : ");
                 score[i][j] = sc.nextInt();
                 sumForEachStudent += score[i][j];
             }
 
-            System.out.println("Average score: " + sumForEachStudent / 3);
+            System.out.println("Average score: " + sumForEachStudent / course);
         }
 
-        System.out.println(" ");
+        System.out.println("\n====================================");
+        System.out.println("      Average Score per Course      ");
+        System.out.println("====================================");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < course; j++) {
             double sumForEachCourse = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < student; i++) {
                 sumForEachCourse += score[i][j];
             }
 
-            System.out.println("Course " + (j + 1) + ": " + (sumForEachCourse / 4));
+            System.out.println("Course " + (j + 1) + ": " + (sumForEachCourse / student));
         }
+        System.out.println("\nProgram finished successfully.");
     }
 }
